9ab25bbe25029d070a7748273b9a4183fedb47be
JeydinNewWon <jaydennguyen6669@gmail.com>
2018-08-23 07:54:35 +1000

    Update schoolOptions.js

--- JSfiles/schoolOptions.js
+++ JSfiles/schoolOptions.js
@@ -1,10 +1,13 @@
 global.$ = require('jquery');
 const qs = require('query-string');
 
+// get the query string from the URL.
 var parsedData = qs.parse(window.location.search);
 var schoolCode = parsedData.schoolCode;
 
+// called when the html is initialised.
 function init() {
+    // edit the button that leads to editSubjects.html, and set it for a query string with a schoolCode.
     $("a[href='editSubjects.html']").attr('href', `editSubjects.html?schoolCode=${schoolCode}`);
 }
 
